f3d16d7f63559eab2bd1e4a657815e6f1e4d21c6
Rajat Rahangdale <rjtrahangdale@gmail.com>
2024-10-12 23:10:25 +0530

    quize app udated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+  ];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState();
+  const [selectedAnswer, setSelectedAnswer] = useState([]);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+  const [isFinish, setFinish] = useState(false);
+
+  const handleNextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    if (currentQuestionIndex === questions.length - 1) {
+      return setFinish(true);
+    } else {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+      setSelectedAnswer(() => {
+        return questions.map(({ questionText, answerOptions }) => {
+          return {
+            questionText,
+            answerOptions: answerOptions.find(({ isCorrect }) => isCorrect),
+          };
+        });
+      });
+    }
+  };
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex === 0) {
+      return;
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isFinish ? (
+        <div className="container">
+          <h1>Game Over!</h1>
+          <p>
+            Your final score is {score} out of {questions.length}.
+          </p>
+          <button
+            className="btn"
+            onClick={() => {
+              setFinish(false);
+              console.log(selectedAnswer);
+              setCurrentQuestionIndex(0);
+            }}
+          >
+            Restart
+          </button>
+        </div>
+      ) : (
+        <div className="container">
+          <h1 className="headline-1">Quize App</h1>
+          <div className="box">
+            <div className="que">
+              <h3 className="">
+                <span>{currentQuestionIndex + 1}. &nbsp; </span>
+                {questions[currentQuestionIndex].questionText}
+              </h3>
+              {questions[currentQuestionIndex].answerOptions.map(
+                ({ answerText }, index) => {
+                  return (
+                    <p
+                      onClick={() => {
+                        setSelectedAnswerIndex(index);
+                      }}
+                      key={index}
+                      style={{
+                        backgroundColor:
+                          selectedAnswerIndex === index && "green",
+                      }}
+                    >
+                      {answerText}
+                    </p>
+                  );
+                }
+              )}
+            </div>
+            <div className="footer">
+              <button
+                disabled={currentQuestionIndex === 0}
+                onClick={handlePreviousQuestion}
+                className="btn prev"
+              >
+                Prev
+              </button>
+              <button
+                onClick={handleNextQuestion}
+                className="btn next"
+                style={{
+                  backgroundColor:
+                    currentQuestionIndex === questions.length - 1 && "green",
+                }}
+              >
+                {currentQuestionIndex === questions.length - 1
+                  ? "Finish"
+                  : "Next"}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
