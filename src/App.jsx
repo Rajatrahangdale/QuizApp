20ca16efc125093463e68cecb6cbe60ab5d15379
Rajat Rahangdale <rjtrahangdale@gmail.com>
2024-10-13 21:53:28 +0530

    quiz app updated again

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 export default function App() {
-  // Define a state variable here to track question status
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [quizFinished, setQuizFinished] = useState(false);
+
   const questions = [
     {
       questionText: "What is the capital of France?",
@@ -44,29 +47,24 @@ export default function App() {
   ];
 
   function handleAnswerClick(correct) {
-    // Check if correct answer is pressed. (See the hint on the left)
     if (correct) {
       setScore((prev) => prev + 1);
     }
     if (currentIndex === questions.length - 1) {
-      // quiz over
       setQuizFinished(true);
     } else {
       setCurrentIndex((value) => value + 1);
     }
+    setSelectedAnswer(null); 
+    setSelectedAnswerIndex(null);
   }
-
-  const [quizFinished, setQuizFinished] = useState(false);
-
-  // Create a state variable here [score, setScore]
 
   return (
     <div className="app">
       {quizFinished ? (
-        /* Change this hardcoded 1 to state variable score else */
         <div className="score-section">
           You scored {score} out of {questions.length}
-          <div className="">
+          <div>
             <button
               onClick={() => {
                 setCurrentIndex(0);
@@ -89,17 +87,30 @@ export default function App() {
             </div>
           </div>
           <div className="answer-section">
-            {questions[currentIndex].answerOptions.map((answer) => {
-              // Add onClick listener to this button
-              return (
-                <button
-                  onClick={() => handleAnswerClick(answer.isCorrect)}
-                  key={answer.answerText}
+            {questions[currentIndex].answerOptions.map((answer, index) => (
+              <div key={index}>
+                <p
+                  style={{
+                    backgroundColor: selectedAnswerIndex === index && "#555e7d",
+                    boxShadow:
+                      selectedAnswerIndex === index &&
+                      "0px 0px 10px 2px rgba(0, 0, 0, 0.5)",
+                  }}
+                  onClick={() => {
+                    setSelectedAnswer(answer.isCorrect);
+                    setSelectedAnswerIndex(index);
+                  }}
                 >
-                  {answer.answerText}
-                </button>
-              );
-            })}
+                  <span>{index + 1}</span> {answer.answerText}
+                </p>
+              </div>
+            ))}
+            <button
+              onClick={()=>handleAnswerClick(selectedAnswer)}
+              disabled={selectedAnswer === null}
+            >
+              Next
+            </button>
           </div>
         </>
       )}
